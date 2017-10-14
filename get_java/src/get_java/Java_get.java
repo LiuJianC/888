a17baeddeafd3fbcdf18c5e24436b4ea41b8d7b2
liujian <Jian1197919728@163.com>
2017-10-14 14:51:35 +0800

    sf

--- get_java/src/get_java/Java_get.java
+++ get_java/src/get_java/Java_get.java
@@ -1,8 +1,15 @@
 package get_java;
 
+import java.io.PrintStream;
+
 public class Java_get {
 
 	public static void main(String[] args) {
 		System.err.println("git∫√¡À");
+		System.out.println("sf");
+		System.err.println("jj");
+		System.err.println("fweg");
+		PrintStream out = System.out;
+		out.append((char) 0);
 	}
 }
